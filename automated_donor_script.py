ff7390bb376a4bda00622a65cd3f435d6c408d8e
Coding89 <timothy.lefevre1@gmail.com>
2026-08-13 17:23:02 +0100

    Commit summary of .py script

---
+++ automated_donor_script.py
@@ -0,0 +1,6 @@
+"""
+This script models how a CRM or webhook integrates automatically dispatches personalised thank you emails to donors.
+This ensures that no donor goes unacknowledged past the charity's SLA target (which can be adjusted in the script).
+
+In other words and in non-technical terms, this script automates thank you emails to donors ensuring that donors are thanked within a charity's turnaround target.
+"""
